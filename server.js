23d4eae7556efc48f5bf040724ab46e1ea9ddcdf
alireza shahriyari <shahriyari@gmail.com>
2015-02-15 22:07:21 +0000

    Modified the port settings.

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ var server = http.createServer(function(request, response) {
 });
 
 
-server.listen(3000, function() {
+server.listen(process.env.PORT || 3000, function() {
 	console.log("Server listening on port 3000.");
 });
 
